03a1f09825e196248e4a4c704aad188e46c40206
Aatmaj Patro <aatmajpatro@gmail.com>
2026-09-02 18:55:20 +0530

    Refactor agents.py to ensure proper loading of environment variables and initialize ChatMistralAI model

--- agents.py
+++ agents.py
@@ -4,3 +4,7 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from tools import web_search , scrape_url 
 from dotenv import load_dotenv
+load_dotenv()
+
+llm=ChatMistralAI(
+    model="mistral-small-2603",temperature=0)

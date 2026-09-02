aab1c2e4e1f627f14fb9ed2d9be2b4366ae34179
Aatmaj Patro <aatmajpatro@gmail.com>
2026-09-02 16:57:55 +0530

    Fix import statement and add web_search function for querying recent information

--- tools.py
+++ tools.py
@@ -1,4 +1,4 @@
-from langchain.tools import Tool
+from langchain.tools import tool
 import requests
 from bs4 import BeautifulSoup
 from tavily import TavilyClient
@@ -9,3 +9,17 @@ load_dotenv()
 
 tavily=TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
 
+@tool
+def web_search(query: str) -> str:
+     """Search the web for recent and reliable information on a topic . Returns Titles , URLs and snippets."""
+     results = tavily.search(query=query, max_results=3)
+
+     out = []
+
+     for r in results['results']:
+         out.append(
+             f"Title: {r['title']}\nURL: {r['url']}\nSnippet: {r['content'][:300]}\n"
+         )
+    
+     return "\n----\n".join(out)
+print(web_search("What is the latest news on war?"))

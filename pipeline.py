056d71de23b592ee607a5adf2b73ca27ab129db4
Aatmaj Patro <aatmajpatro@gmail.com>
2026-09-04 14:43:49 +0530

    Fix formatting and add print statements in run_research_pipeline function

--- pipeline.py
+++ pipeline.py
@@ -5,6 +5,8 @@ from agents import build_search_agent, build_reader_agent, writer_chain, critic_
 # the reader agent to scrape content from URLs, and then generates a research report and critique. The function returns a dictionary containing the research report, critique, and the sources used.
 
 def run_research_pipeline(topic: str)-> dict:
-    state={}
+    state = {}
     # Step 1: Search agent workjng
-    
+    print("\n"+" ="*50)
+    print("step 1 - search agent is working ...")
+    print("="*50)
